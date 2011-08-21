6f7c953e899f2267c932bd1d78b2da2e3ec8e954
Adrian Cheng Bing Jie <adriancbjie@gmail.com>
2011-08-21 21:36:38 +0800

    adding Locations prototype as basic CodeData Sample

---
+++ Prototypes/Locations/Locations/Event.m
@@ -0,0 +1,17 @@
+//
+//  Event.m
+//  Locations
+//
+//  Created by Adrian on 11/8/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Event.h"
+
+
+@implementation Event
+@dynamic creationDate;
+@dynamic latitude;
+@dynamic longitude;
+
+@end
---
+++ Prototypes/Locations/Locations/iPad/LocationsAppDelegate_iPad.m
@@ -0,0 +1,13 @@
+//
+//  LocationsAppDelegate_iPad.m
+//  Locations
+//
+//  Created by Adrian on 10/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LocationsAppDelegate_iPad.h"
+
+@implementation LocationsAppDelegate_iPad
+
+@end
