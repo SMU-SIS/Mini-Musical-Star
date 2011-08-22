beb50751ddf5abe0c5f63345d78b44c802c7ee50
Wei Jie Tan <weijie.tan.2009@gmail.com>
2011-08-22 18:55:39 +0800

    Added Graphics Controller View for Toggle menu

---
+++ Prototypes/UI/MMS-UI/MMS-UI/Graphics.h
@@ -0,0 +1,16 @@
+//
+//  Graphics.h
+//  MMS-UI
+//
+//  Created by Weijie Tan on 8/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface Graphics : UIViewController {
+    
+}
+
+@end
