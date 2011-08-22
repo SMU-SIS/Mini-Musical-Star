5f407cae6b158d22c6a3d9c1616408b5ee4cc3a1
Adrian Cheng Bing Jie <adriancbjie@gmail.com>
2011-08-22 18:04:13 +0800

    starting out relationships coredata

---
+++ MusicalStar/MusicalStarTests/MusicalStarTests.h
@@ -0,0 +1,13 @@
+//
+//  MusicalStarTests.h
+//  MusicalStarTests
+//
+//  Created by Adrian on 22/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MusicalStarTests : SenTestCase
+
+@end
