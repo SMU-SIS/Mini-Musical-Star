f8158c68560fe8cd6169aba2c27b059af399a207
Lee Jun Kit <mohawk.riceball@gmail.com>
2011-08-28 16:48:24 +0800

    started writing ShowDAO.h

---
+++ MusicalStar/MusicalStar/ShowDAO.h
@@ -0,0 +1,13 @@
+//
+//  ShowDAO.h
+//  MusicalStar
+//
+//  Created by Jun Kit Lee on 28/8/11.
+//  Copyright 2011 mohawk.riceball@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShowDAO : NSObject
+
+@end
