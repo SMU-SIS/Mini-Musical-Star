2af228f9b61808c6f3aeb653728e636b011b3fe0
Lee Jun Kit <mohawk.riceball@gmail.com>
2011-09-08 14:22:45 +0800

    created a new prototype for andy ui recommendations

---
+++ Prototypes/CrollUI/CrollUI/PhotoEditorViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoEditorViewController.h
+//  CrollUI
+//
+//  Created by Jun Kit Lee on 8/9/11.
+//  Copyright 2011 mohawk.riceball@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoEditorViewController : UIViewController
+
+@end
---
+++ Prototypes/CrollUI/CrollUITests/CrollUITests.h
@@ -0,0 +1,13 @@
+//
+//  CrollUITests.h
+//  CrollUITests
+//
+//  Created by Jun Kit Lee on 8/9/11.
+//  Copyright 2011 mohawk.riceball@gmail.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CrollUITests : SenTestCase
+
+@end
