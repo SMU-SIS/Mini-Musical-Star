9011caea6df8d4469073dc34a968494ef9e0ef7d
Adrian Cheng Bing Jie <adriancbjie@gmail.com>
2011-09-08 11:55:14 +0800

    PhotoEditView doing the new UI view after andy crolls comments

---
+++ Prototypes/UI/MMS-UI/MMS-UI/PhotoEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoEditViewController.h
+//  MMS-UI
+//
+//  Created by Adrian on 7/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoEditViewController : UIViewController
+
+@end
