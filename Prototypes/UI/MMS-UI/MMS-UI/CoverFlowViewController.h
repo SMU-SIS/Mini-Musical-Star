f08c30e4f8511e9e6aa8780604cb61f94cd77992
Adrian Cheng Bing Jie <adriancbjie@gmail.com>
2011-09-15 19:00:44 +0800

    changed the flow of photo to coverflow but yet to make it dynamic to play slider

---
+++ Prototypes/UI/MMS-UI/MMS-UI/CoverFlowViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoverFlowViewController.h
+//  MMS-UI
+//
+//  Created by Adrian on 15/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoverFlowViewController : UIViewController
+
+@end
