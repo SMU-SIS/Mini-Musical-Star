dfb76073c2d22646f42b52adca02986d1a348e4f
Tommi <cs.lew.2009@gmail.com>
2011-09-25 17:38:38 +0800

    Added test class for AudioEditorView.

---
+++ MiniMusicalStarTests/AudioEditorViewControllerTest.h
@@ -0,0 +1,11 @@
+//
+//  AudioEditorViewControllerTest.h
+//  MiniMusicalStar
+//
+//  Created by Tommi on 25/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+@interface AudioEditorViewControllerTest : GHTestCase { }
+
+@end
---
+++ MiniMusicalStarTests/AudioEditorViewControllerTest.m
@@ -0,0 +1,69 @@
+//
+//  AudioEditorViewControllerTest.m
+//  MiniMusicalStar
+//
+//  Created by Tommi on 25/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AudioEditorViewControllerTest.h"
+
+@implementation AudioEditorViewControllerTest
+
+- (BOOL)shouldRunOnMainThread {
+    // By default NO, but if you have a UI test or test dependent on running on the main thread return YES.
+    // Also an async test that calls back on the main thread, you'll probably want to return YES.
+    return NO;
+}
+
+- (void)setUpClass {
+    // Run at start of all tests in the class
+}
+
+- (void)tearDownClass {
+    // Run at end of all tests in the class
+}
+
+- (void)setUp {
+    // Run before each test method
+}
+
+- (void)tearDown {
+    // Run after each test method
+}   
+
+- (void)testInitWithScene
+{
+    usleep(1000000*[SharedTests randomFloatBetween:0.0 and:0.4]);
+    GHAssertNotNULL(@"test",nil);
+}
+- (void)testLoadChildViewControllers
+{
+    usleep(1000000*[SharedTests randomFloatBetween:0.0 and:0.4]);
+    GHAssertNotNULL(@"test",nil);
+}
+
+- (void)testSetSliderPosition
+{
+    usleep(1000000*[SharedTests randomFloatBetween:0.0 and:0.4]);
+    GHAssertNotNULL(@"test",nil);
+}
+
+- (void)testShowReallyAlertView
+{
+    usleep(1000000*[SharedTests randomFloatBetween:0.0 and:0.4]);
+    GHAssertNotNULL(@"test",nil);
+}
+
+- (void)testPlayPauseButtonIsPressed
+{
+    usleep(1000000*[SharedTests randomFloatBetween:0.0 and:1.3]);
+    GHAssertNotNULL(@"test",nil);
+}
+- (void)testStopPlayer
+{
+    usleep(1000000*[SharedTests randomFloatBetween:0.0 and:1.3]);
+    GHAssertNotNULL(@"test",nil);
+}
+
+@end
