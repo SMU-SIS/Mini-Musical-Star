ff463d309b227d6e1bd0039bf426349c43951c67
Adrian Cheng Bing Jie <adriancbjie@gmail.com>
2011-09-25 11:10:10 +0800

    added a simple sample test case, going to write some test cases today

--- MiniMusicalStarTests/PhotoEditorViewControllerTest.m
+++ MiniMusicalStarTests/PhotoEditorViewControllerTest.m
@@ -2,27 +2,49 @@
 //  PhotoEditorViewControllerTest.m
 //  MiniMusicalStar
 //
-//  Created by Adrian on 23/9/11.
+//  Created by Adrian on 25/9/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#import <GHUnitIOS/GHUnit.h> 
-
-@interface PhotoEditorViewControllerTest : GHTestCase { }
-@end
-
+#import "PhotoEditorViewControllerTest.h"
 @implementation PhotoEditorViewControllerTest
 
-- (void)testStrings {       
-    NSString *string1 = @"a string";
-    GHTestLog(@"I can log to the GHUnit test console: %@", string1);
+- (BOOL)shouldRunOnMainThread {
+    // By default NO, but if you have a UI test or test dependent on running on the main thread return YES.
+    // Also an async test that calls back on the main thread, you'll probably want to return YES.
+    return NO;
+}
+
+- (void)setUpClass {
+    // Run at start of all tests in the class
+}
+
+- (void)tearDownClass {
+    // Run at end of all tests in the class
+}
+
+- (void)setUp {
+    // Run before each test method
+}
+
+- (void)tearDown {
+    // Run after each test method
+}   
+
+- (void)testFoo {       
+    NSString *a = @"foo";
+    GHTestLog(@"I can log to the GHUnit test console: %@", a);
     
-    // Assert string1 is not NULL, with no custom error description
-    GHAssertNotNULL(string1, nil);
+    // Assert a is not NULL, with no custom error description
+    GHAssertNotNULL(a, nil);
     
     // Assert equal objects, add custom error description
-    NSString *string2 = @"a string";
-    GHAssertEqualObjects(string1, string2, @"A custom error message. string1 should be equal to: %@.", string2);
+    NSString *b = @"bar";
+    GHAssertEqualObjects(a, b, @"A custom error message. a should be equal to: %@.", b);
+}
+
+- (void)testBar {
+    // Another test
 }
 
 @end
