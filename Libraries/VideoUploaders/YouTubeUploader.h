b10e0301e45e9d82feca70fade9be298ae3e2b62
Tommi <cs.lew.2009@gmail.com>
2011-10-23 18:06:59 +0800

    added the GData Objective-C API for youtube uploading

---
+++ Libraries/VideoUploaders/YouTubeUploader.h
@@ -0,0 +1,21 @@
+//
+//  YouTubeUploader.h
+//  MiniMusicalStar
+//
+//  Created by Tommi on 23/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "GDataServiceGoogleYouTube.h"
+#import "GDataEntryPhotoAlbum.h"
+#import "GDataEntryPhoto.h"
+#import "GDataFeedPhoto.h"
+#import "GDataEntryYouTubeUpload.h"
+
+#import "GDataYouTubeConstants.h"
+
+@interface YouTubeUploader : NSObject
+
+@end
---
+++ Libraries/VideoUploaders/YouTubeUploader.m
@@ -0,0 +1,13 @@
+//
+//  YouTubeUploader.m
+//  MiniMusicalStar
+//
+//  Created by Tommi on 23/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "YouTubeUploader.h"
+
+@implementation YouTubeUploader
+
+@end
