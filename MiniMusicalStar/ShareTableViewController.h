f29f82f923114cdcccf69a5502dfe3aecaca7e5a
Tommi <cs.lew.2009@gmail.com>
2011-10-30 23:22:45 +0800

    added a new table view controller to display the exported content.

---
+++ MiniMusicalStar/ShareTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareTableViewController.h
+//  MiniMusicalStar
+//
+//  Created by Tommi on 30/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShareTableViewController : UITableViewController
+
+@end
