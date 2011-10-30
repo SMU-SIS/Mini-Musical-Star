70b445ed9dd32a2c6f58c9fb1eeebbe44500b80e
Tommi <cs.lew.2009@gmail.com>
2011-10-30 18:44:57 +0800

    adding images and export view controller

---
+++ MiniMusicalStar/ExportViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExportViewController.h
+//  MiniMusicalStar
+//
+//  Created by Tommi on 30/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExportViewController : UIViewController
+
+@end
