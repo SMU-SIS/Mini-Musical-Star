6e6935947fb26dd74ec509ad32e3a2459b7ed880
Tommi <cs.lew.2009@gmail.com>
2011-10-31 22:58:49 +0800

    adding back class.

---
+++ MiniMusicalStar/YouTubeUploaderViewController.h
@@ -0,0 +1,13 @@
+//
+//  YouTubeUploaderViewController.h
+//  MiniMusicalStar
+//
+//  Created by Tommi on 31/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YouTubeUploaderViewController : UIViewController
+
+@end
