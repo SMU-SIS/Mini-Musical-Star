fd7d72e2d023262c23b44535aae3bde76a68b3a4
Lee Jun Kit <mohawk.riceball@gmail.com>
2011-11-03 14:47:17 +0800

    made the space between the musicals bigger

--- MiniMusicalStar/MenuViewController.m
+++ MiniMusicalStar/MenuViewController.m
@@ -68,7 +68,7 @@
     scrollView.showsVerticalScrollIndicator = NO;
     scrollView.pagingEnabled = NO;
     scrollView.clipsToBounds = NO;    
-    [scrollView setContentSize:CGSizeMake(self.showDAO.loadedShows.count * 280, scrollView.frame.size.height)];
+    [scrollView setContentSize:CGSizeMake(self.showDAO.loadedShows.count * (280 + 125), scrollView.frame.size.height)];
     
     //create a buttonArray as the backing for the buttons in the scrollview (so we can refer to them later)
     self.buttonArray = [NSMutableArray arrayWithCapacity:self.showDAO.loadedShows.count];
@@ -77,7 +77,7 @@
     [self.showDAO.loadedShows enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
         
         CGRect frame;
-        frame.origin.x = scrollView.frame.size.width/3 * idx;
+        frame.origin.x = (280 + 125) * idx;
         frame.origin.y = 0;
         frame.size.width = 280;
         frame.size.height = scrollView.frame.size.height;
