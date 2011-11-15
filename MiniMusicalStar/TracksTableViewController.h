f791d86226df364de7a4f6fed93c694464e2fe82
Tommi <cs.lew.2009@gmail.com>
2011-11-15 16:46:41 +0800

    created two new view controllers for audio view refactoring.

---
+++ MiniMusicalStar/TracksTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TracksTableViewController.h
+//  MiniMusicalStar
+//
+//  Created by Tommi on 15/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TracksTableViewController : UITableViewController
+
+@end
